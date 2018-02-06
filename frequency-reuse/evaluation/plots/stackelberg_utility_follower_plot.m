ce8db5d2a7f5daff4e19db2f3c4ff803a7358dee
Sebastian Lindner <sebastian.lindner@tuhh.de>
2018-02-06 12:15:42 +0100

    follower utility plot

--- frequency-reuse/evaluation/plots/stackelberg_utility_follower_plot.m
+++ frequency-reuse/evaluation/plots/stackelberg_utility_follower_plot.m
@@ -1,6 +1,6 @@
 p_max = 400;
 p_min = 1;
-p_tx = 398;
+p_k = 398;
 g_ke = 1e-8;
 g_ki = 1e-8;
 g_ik = 1e-8;
@@ -8,18 +8,27 @@ g_ie = 1e-8;
 g_ii = 1e-8;
 N = 4e-18;
 beta = 2;
-price = 200000;
+price1 = 4e5;
+price2 = 0.5 * price1;
+price3 = 0.5 * price2;
 
-utilVec = [];
+utilVec1 = [];
+utilVec2 = [];
+utilVec3 = [];
 pRange = 0:1:1000;
 for i=1:size(pRange, 2)
     p = pRange(i);
-    utilVec(end+1) = stackelberg_utility_follower(price, p, p_tx, g_ii, g_ki, g_ie, N);
+    utilVec1(end+1) = stackelberg_utility_follower(price1, p, p_k, g_ii, g_ki, g_ie, N);
+    utilVec2(end+1) = stackelberg_utility_follower(price2, p, p_k, g_ii, g_ki, g_ie, N);
+    utilVec3(end+1) = stackelberg_utility_follower(price3, p, p_k, g_ii, g_ki, g_ie, N);
 end
 
 figure;
 hold on;
-plot(pRange, utilVec);
+plot(pRange, utilVec1);
+plot(pRange, utilVec2);
+plot(pRange, utilVec3);
+legend({strcat('\alpha_k=', int2str(price1)), strcat('\alpha_k=', int2str(price2)), strcat('\alpha_k=', int2str(price3))}, 'Location', 'northwest')
 ylabel('u_i');
 xlabel('p_i [mW]');
 set(gca,'FontSize', 26);
