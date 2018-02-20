bd86e050d66bd3e1a5bc0495f04e54e95cd1f93a
kunterbunt <to-kunterbunt@gmx.net>
2018-02-20 18:57:26 +0100

    various plots

--- frequency-reuse/evaluation/plots/analysis_stackelcompare_tu_modded_p_study.m
+++ frequency-reuse/evaluation/plots/analysis_stackelcompare_tu_modded_p_study.m
@@ -3,32 +3,58 @@ n = 10;
 pVec = 0:0.1:1;
 filenamesD2D = {};
 filenamesCell = {};
+filenamesTraditionalGames = {};
+filenamesReverseGames = {};
+filenamesD2DGames = {};
 
 for i=1:size(pVec,2)
     p = pVec(i);
     for rep=0:(reps - 1)                                
         filenamesD2D(i, rep+1) = {strcat('../../results/stackelcompare_tu_modded_p_study/n', int2str(n), '_p', num2str(p), '_rep', int2str(rep), '.sca.d2d')};   
         filenamesCell(i, rep+1) = {strcat('../../results/stackelcompare_tu_modded_p_study/n', int2str(n), '_p', num2str(p), '_rep', int2str(rep), '.sca.cell')};   
+        filenamesTraditionalGames(i, rep+1) = {strcat('../../results/stackelcompare_tu_modded_p_study/n', int2str(n), '_p', num2str(p), '_rep', int2str(rep), '.sca.traditgames')};   
+        filenamesReverseGames(i, rep+1) = {strcat('../../results/stackelcompare_tu_modded_p_study/n', int2str(n), '_p', num2str(p), '_rep', int2str(rep), '.sca.reversegames')};   
+        filenamesD2DGames(i, rep+1) = {strcat('../../results/stackelcompare_tu_modded_p_study/n', int2str(n), '_p', num2str(p), '_rep', int2str(rep), '.sca.d2dgames')};   
     end    
 end
 
 throughputMatD2D = zeros(size(pVec, 2), 3);
 throughputMatCell = zeros(size(pVec, 2), 3);
+throughputMatCombined = zeros(size(pVec, 2), 3);
+numTraditionalGamesMat = zeros(size(pVec, 2), 3);
+numReverseGamesMat = zeros(size(pVec, 2), 3);
+numD2DGamesMat = zeros(size(pVec, 2), 3);
  
 for i=1:size(pVec, 2)
     [throughputMatD2D(i, 1), throughputMatD2D(i, 2), throughputMatD2D(i, 3)] = getMeanVec(filenamesD2D(i,:));    
     [throughputMatCell(i, 1), throughputMatCell(i, 2), throughputMatCell(i, 3)] = getMeanVec(filenamesCell(i,:));        
+    [throughputMatCombined(i, 1), throughputMatCombined(i, 2), throughputMatCombined(i, 3)] = getMeanVec([filenamesCell(i,:) filenamesD2D(i,:)]);         
+    [numTraditionalGamesMat(i, 1), numTraditionalGamesMat(i, 2), numTraditionalGamesMat(i, 3)] = getMeanVec(filenamesTraditionalGames(i,:));         
+    [numReverseGamesMat(i, 1), numReverseGamesMat(i, 2), numReverseGamesMat(i, 3)] = getMeanVec(filenamesReverseGames(i,:));         
+    [numD2DGamesMat(i, 1), numD2DGamesMat(i, 2), numD2DGamesMat(i, 3)] = getMeanVec(filenamesD2DGames(i,:));         
 end
 
 figure;
 hold on;
 ylabel({'Overall';'Mean Opinion Score'});
-% ylim([3 4.5]);
 errorbar(pVec, throughputMatCell(:, 1), throughputMatCell(:, 2), throughputMatCell(:, 3), '--v', 'LineWidth', 2);
 errorbar(pVec, throughputMatD2D(:, 1), throughputMatD2D(:, 2), throughputMatD2D(:, 3), '--v', 'LineWidth', 2);
-% xticks(N);
-% xticklabels(N);
-legend({'Cellular VoIP users', 'D2D VoIP users'}, 'Location', 'east', 'FontSize', 26);
+% errorbar(pVec, throughputMatCombined(:, 1), throughputMatCombined(:, 2), throughputMatCombined(:, 3), ':', 'LineWidth', 2);
+plot(pVec, throughputMatCombined(:, 1), ':', 'LineWidth', 2, 'Color', 'black');
+xticks(pVec);
+xlabel('D2D penalty p');
+legend({'Cellular VoIP users', 'D2D VoIP users', 'both'}, 'Location', 'northeast', 'FontSize', 26);
 set(gca,'FontSize', 26);
+hold off;
 
+figure;
+hold on;
+errorbar(pVec, numTraditionalGamesMat(:, 1), numTraditionalGamesMat(:, 2), numTraditionalGamesMat(:, 3), '--v', 'LineWidth', 2);
+errorbar(pVec, numReverseGamesMat(:, 1), numReverseGamesMat(:, 2), numReverseGamesMat(:, 3), '--v', 'LineWidth', 2);
+errorbar(pVec, numD2DGamesMat(:, 1), numD2DGamesMat(:, 2), numD2DGamesMat(:, 3), '--v', 'LineWidth', 2);
+xticks(pVec);
+xlabel('D2D penalty p');
+ylabel('Count [#]');
+legend({'traditional games', 'reverse games', 'D2D games'}, 'Location', 'northeast', 'FontSize', 26);
+set(gca,'FontSize', 26);
 hold off;
--- frequency-reuse/evaluation/plots/analysis_vid_vs_fulbuf_d2d.m
+++ frequency-reuse/evaluation/plots/analysis_vid_vs_fulbuf_d2d.m
@@ -3,10 +3,12 @@ N = 4:2:10;
 filenamesRR = {};
 filenamesPF = {};
 filenamesTU = {};
+filenamesSTA = {};
 
 filenamesRR_fulbuf = {};
 filenamesPF_fulbuf = {};
 filenamesTU_fulbuf = {};
+filenamesSTA_fulbuf = {};
 
 for i=1:size(N,2)
     n = N(i);
@@ -14,29 +16,35 @@ for i=1:size(N,2)
         filenamesRR(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-RR/n', int2str(n), '_rep', int2str(rep), '.sca.vid')};   
         filenamesPF(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-PF/n', int2str(n), '_rep', int2str(rep), '.sca.vid')};           
         filenamesTU(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-TU/n', int2str(n), '_rep', int2str(rep), '.sca.vid')};           
+        filenamesSTA(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-STA/n', int2str(n), '_rep', int2str(rep), '.sca.vid')};           
         
         filenamesRR_fulbuf(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-RR/n', int2str(n), '_rep', int2str(rep), '.sca.fulbuf')};   
         filenamesPF_fulbuf(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-PF/n', int2str(n), '_rep', int2str(rep), '.sca.fulbuf')};           
         filenamesTU_fulbuf(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-TU/n', int2str(n), '_rep', int2str(rep), '.sca.fulbuf')};           
+        filenamesSTA_fulbuf(i, rep+1) = {strcat('../../results/TU-VID-VS-FULBUF-STA/n', int2str(n), '_rep', int2str(rep), '.sca.fulbuf')};           
     end    
 end
 
 vidMatRR = zeros(size(N, 2), 3);
 vidMatPF = zeros(size(N, 2), 3);
 vidMatTU = zeros(size(N, 2), 3);
+vidMatSTA = zeros(size(N, 2), 3);
 
 fulbufMatRR = zeros(size(N, 2), 3);
 fulbufMatPF = zeros(size(N, 2), 3);
 fulbufMatTU = zeros(size(N, 2), 3);
+fulbufMatSTA = zeros(size(N, 2), 3);
  
 for i=1:size(N, 2)
     [vidMatRR(i, 1), vidMatRR(i, 2), vidMatRR(i, 3)] = getMeanVec(filenamesRR(i,:));    
     [vidMatPF(i, 1), vidMatPF(i, 2), vidMatPF(i, 3)] = getMeanVec(filenamesPF(i,:));        
     [vidMatTU(i, 1), vidMatTU(i, 2), vidMatTU(i, 3)] = getMeanVec(filenamesTU(i,:));       
+    [vidMatSTA(i, 1), vidMatSTA(i, 2), vidMatSTA(i, 3)] = getMeanVec(filenamesSTA(i,:));       
     
     [fulbufMatRR(i, 1), fulbufMatRR(i, 2), fulbufMatRR(i, 3)] = getMeanVec(filenamesRR_fulbuf(i,:));    
     [fulbufMatPF(i, 1), fulbufMatPF(i, 2), fulbufMatPF(i, 3)] = getMeanVec(filenamesPF_fulbuf(i,:));        
-    [fulbufMatTU(i, 1), fulbufMatTU(i, 2), fulbufMatTU(i, 3)] = getMeanVec(filenamesTU_fulbuf(i,:));       
+    [fulbufMatTU(i, 1), fulbufMatTU(i, 2), fulbufMatTU(i, 3)] = getMeanVec(filenamesTU_fulbuf(i,:));
+    [fulbufMatSTA(i, 1), fulbufMatSTA(i, 2), fulbufMatSTA(i, 3)] = getMeanVec(filenamesSTA_fulbuf(i,:));
 end
 
 figure;
@@ -47,7 +55,9 @@ ylabel({'throughput sum';'3 video streamers'});
 errorbar(N, vidMatPF(:, 1), vidMatPF(:, 2), vidMatPF(:, 3), '--v', 'LineWidth', 2);
 errorbar(N, vidMatRR(:, 1), vidMatRR(:, 2), vidMatRR(:, 3), '--v', 'LineWidth', 2);
 errorbar(N, vidMatTU(:, 1), vidMatTU(:, 2), vidMatTU(:, 3), '--v', 'LineWidth', 2);
-legend({'Round Robin', 'Proportional Fair', 'Transferable Utility'}, 'Location','northeast');
+errorbar(N, vidMatSTA(:, 1), vidMatSTA(:, 2), vidMatSTA(:, 3), '--v', 'LineWidth', 2);
+% legend({'Round Robin', 'Proportional Fair', 'Transferable Utility'}, 'Location','northeast');
+legend({'Round Robin', 'Proportional Fair', 'Transferable Utility', 'Joint Scheduler'}, 'Location','northeast');
 xticklabels({'4', '', '6', '', '8', '', '10'});
 set(gca,'FontSize', 26);
 
@@ -59,7 +69,9 @@ xlabel('number of D2D full-buffer pairs n [#]');
 errorbar(N, fulbufMatRR(:, 1), fulbufMatRR(:, 2), fulbufMatRR(:, 3), '--v', 'LineWidth', 2);
 errorbar(N, fulbufMatPF(:, 1), fulbufMatPF(:, 2), fulbufMatPF(:, 3), '--v', 'LineWidth', 2);
 errorbar(N, fulbufMatTU(:, 1), fulbufMatTU(:, 2), fulbufMatTU(:, 3), '--v', 'LineWidth', 2);
-legend({'Round Robin', 'Proportional Fair', 'Transferable Utility'}, 'Location','northeast');
+errorbar(N, fulbufMatSTA(:, 1), fulbufMatSTA(:, 2), fulbufMatSTA(:, 3), '--v', 'LineWidth', 2);
+% legend({'Round Robin', 'Proportional Fair', 'Transferable Utility'}, 'Location','northeast');
+legend({'Round Robin', 'Proportional Fair', 'Transferable Utility', 'Joint Scheduler'}, 'Location','northeast');
 xticklabels({'4', '', '6', '', '8', '', '10'});
 set(gca,'FontSize', 26);
 hold off;
