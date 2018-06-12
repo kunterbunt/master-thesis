e3028ea9df36bedd3ad3fcf48bb725d94466f6cc
Sebastian Lindner <sebastian.lindner@tuhh.de>
2018-06-12 14:35:34 +0200

    implemented free space path loss model, config_channel.xml uses it now

--- frequency-reuse/evaluation/interference_calculation.m
+++ frequency-reuse/evaluation/interference_calculation.m
@@ -1,8 +1,8 @@
 p_trans = 26; % dBm
 N = -174; % dB
-dVec = 325:20:465; % Meters.
+dVec = 5:5:100; % Meters.
 dVec = dVec / 1000; % Kilometers.
-d_pair = 10 / 1000; % Meters between transmitter and receiver.
+d_pair = 30 / 1000; % Kilometers between transmitter and receiver.
 frequency = 2.4; % GHz.
 
 att_link = getPathAttenuation(d_pair, frequency);
@@ -17,5 +17,6 @@ for i = 1 : size(dVec, 2)
     sinrVec(i) = 10^(p_rcvd/10) / (10^(interferenceVec(i)/10) + 10^(N/10));
 end
 
-sinrVec = 10*log10(sinrVec)
+%sinrVec = 10*log10(sinrVec)
+10*log10(interferenceVec)
 
--- frequency-reuse/evaluation/plots/analysis_incell_interference.m
+++ frequency-reuse/evaluation/plots/analysis_incell_interference.m
@@ -1,5 +1,7 @@
+%% Parse simulation results.
 reps = 20;
-dVec = 420:20:500;
+dVec = 405:5:500;
+location = 400;
 filenames = {};
 
 for i=1:size(dVec,2)
@@ -15,11 +17,37 @@ for i=1:size(dVec, 2)
     [interferenceMat(i, 1), interferenceMat(i, 2), interferenceMat(i, 3)] = getMeanVec(filenames(i,:));    
 end
 
+%% Obtain analytical expectation.
+p_trans = 26; % dBm
+N = -174; % dB
+d_pair = 30; % Meters between transmitter and receiver.
+frequency = 2; % GHz.
+
+att_link = getPathAttenuation(d_pair / 1000, frequency);
+p_rcvd = p_trans - att_link;
+gain = p_rcvd - p_trans;
+
+interferenceVec = [];
+sinrVec = [];
+for i = 1 : size(dVec, 2)
+    att_interference = getPathAttenuation((dVec(i) - location) / 1000, frequency);
+    interferenceVec(i) = p_trans - att_interference;        
+    sinrVec(i) = p_rcvd / interferenceVec(i) + N;
+end
+
+%% Plot. 
 figure;
 hold on;
-ylabel('in-cell interference');
-errorbar(dVec - dVec(1), interferenceMat(:, 1), interferenceMat(:, 2), interferenceMat(:, 3), '--v', 'LineWidth', 2);
+
+% yyaxis left;
+ylabel('in-cell interference [dB]');
+errorbar(dVec - location, 10*log10(interferenceMat(:, 1)), 10*log10(interferenceMat(:, 2)), 10*log10(interferenceMat(:, 3)), '--v', 'LineWidth', 2);
+plot(dVec - location, interferenceVec);
+
+% yyaxis right;
+% plot(dVec - location, sinrVec);
+
 xlabel('distance [m]');
-% legend({'Rx_0', 'Rx_{i>0}', 'Interpreter', 'latex'}, 'Location', 'northeast', 'FontSize', 26);
+legend({'simulation', 'analytical'}, 'Location', 'northeast', 'FontSize', 26);
 set(gca,'FontSize', 26);
 hold off;
